e9a03bc6a77c32b46b99993643af911e52b8ad6b
TJ DeVries <devries.timothyj@gmail.com>
2023-03-28 17:03:36 -0400

    next: test opening

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,14 @@ module WithFunction = struct
   let read () = Simple.nested_func "read"
 end
 
-let () = 
+let read () = "read"
+let _ = read ()
+
+let () =
   let _ = WithFunction.read () in
+  print_endline "done"
+
+let () =
+  let open WithFunction in
+  let _ = read () in
   print_endline "done"

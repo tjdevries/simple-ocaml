38339795af288cf0759a0d1ec2e2a42a17a45d09
TJ DeVries <devries.timothyj@gmail.com>
2023-04-04 14:32:13 -0400

    next: more docs

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,16 @@
 module WithFunction = struct
-  type t = { x : int; y : int }
+  type t = { x : int (** Wow, a very cool integer *);
+             y : string (** This is, indeed, a string *) }
 
   let read () = Simple.nested_func "read"
-  let init () = { x = 1; y = 2 }
+  let init () = { x = 1; y = "default" }
   let add t i = { t with x = t.x + i }
-  let sub t i = { t with y = t.y - i }
+  let concat t str = { t with y = t.y ^ str }
 end
 
 let a = WithFunction.init ()
 let b = WithFunction.add a 1
-let c = WithFunction.sub b 2
+let c = WithFunction.concat b ", updated"
 let _ = c
 
 let read () = "read"
